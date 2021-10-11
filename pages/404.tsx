a8a8ec8343e2a3e438248f1fe3c5e74c5a275281
Xiaoning He <xhe123@hotmail.co.uk>
2021-10-11 15:21:07 +0100

    Homepage basic UI

---
+++ pages/404.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Layout from "../components/_Layout";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  });
+
+  return (
+    <Layout title="page not found">
+      <Typography variant="h1">Oops...</Typography>
+      <Typography variant="h2">That page cannot be found</Typography>
+      <Typography>
+        Go back to the <Link href="/">HomePage</Link>
+      </Typography>
+    </Layout>
+  );
+}
