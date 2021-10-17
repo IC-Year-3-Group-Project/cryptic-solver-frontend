e0ab66bcb4db18b2a32f63d01e05a3adba6559d5
Guolin Yang <guolin.yang13@gmail.com>
2021-10-17 14:31:25 +0100

    Integration test for invalid crossword url added

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,19 +1,3 @@
-describe("Invalid urls bring up sorry message", () => {
-  it("invalid urls should lead to sorry page", () => {
-    // Start from the index page
-    cy.visit("/");
-
-    // Input invalid URL into text box
-    cy.get("input").type("invalid-url{enter}");
-
-    // The new url should include "/crossword"
-    cy.url().should("include", "/crossword");
-
-    // The new page should contain an h1 with "Sorry"
-    cy.get("h1").contains("Sorry");
-  });
-});
-
 describe("Can go back to home page from sorry message", () => {
   it("should navigate back to home page from sorry page", () => {
     // Start from the index page
@@ -31,17 +15,31 @@ describe("Can go back to home page from sorry message", () => {
 });
 
 // TODO test with mocked api request
-// describe("Fetching crossword fails should bring up sorry message", () => {
+describe("Fetching crossword fails should bring up sorry message", () => {
 
-//   // Mock return value to initiate error handling response from the page
-//   cy.intercept('POST', 'https://cryptic-solver-backend.herokuapp.com/fetch-crossword', {})
+  before(() => {
+    // runs once before all tests in the block
+      cy.visit("/");
+    
+      // Input invalid URL into text box
+      cy.get("input").type("invalid-url{enter}");
+    
+      // The new url should include "/crossword"
+      cy.url().should("include", "/crossword");
 
-//   it("should have sorry message", () => {
-//     cy.contains("Sorry your crossword could not be found")
-//   })
+      // Mock return value to initiate error handling response from the page
+      cy.intercept('POST', 'https://cryptic-solver-backend.herokuapp.com/fetch-crossword', {})
+  })
 
-//   it("should have try again link", () => {
-//     cy.contains("Try again").click()
-//   })
 
-// })
+  it("should have sorry message", () => {
+    cy.get('h1').contains("Sorry your crossword could not be found")
+  })
+
+  it("should have try again link", () => {
+    cy.contains("Try again").click()
+    // assert that we are on the home page
+    cy.url().should('eq', Cypress.config().baseUrl + '/')
+  })
+
+})
