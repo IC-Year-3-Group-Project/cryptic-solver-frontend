d9d8604c3288be85daad07438606de46ba51acb1
Zlatina Mileva <zlatina.mileva19@imperial.ac.uk>
2021-10-30 19:35:04 +0300

    Started on clue extraction from the text

--- pages/text.tsx
+++ pages/text.tsx
@@ -12,6 +12,68 @@ const ImagePage: NextPage = () => {
     logger: (m) => console.log(m),
   });
 
+  // this function doesn't really work yet, I think it has an infinite loop
+  const extract_clues = (text: string) => {
+    text = text.substring(6,)
+    let inClue = false
+    let inBrackets = false
+    let clue = ""
+    let clues = []
+    let lengths = []
+    let clueNumber = ""
+    let i = 0
+    while (i < text.length){
+      let character = text[i]
+      if (character == "\n"){
+        i++;
+        continue;
+      }
+      if(inBrackets) {
+        i++;
+        if (character == ")"){
+          inBrackets = false;
+          clues.push({clueNumber: clueNumber, 
+                      clue: clue,
+                      lenghts: lengths,
+                      direction: 0});
+          clueNumber = ""
+          lengths = []
+          clue = ""
+          continue;
+        } else if (character == ",") {
+          continue;
+        } else {
+          lengths.push(character)
+          continue;
+        }
+      } else if(inClue){
+        clue = clue.concat("", character);
+        if (text[i+2] != "(") {
+          inClue = false;
+          i++;
+        }else{
+          i = i + 2
+        }
+      } else if (character == " "){
+        i++;
+        continue;
+      } else if (character == "("){
+        inBrackets = true;
+        i++;
+        continue;
+      } else {
+        clueNumber = clueNumber.concat("", character)
+        if(text[i + 1] == " ") {
+          inClue = true;
+          i = i + 2;
+        } else{
+          i++;
+        }
+      }
+    }
+    console.log(clues)
+  }
+
   //@ts-ignore
   useEffect(async () => {
     console.log("loading...");
@@ -22,13 +84,14 @@ const ImagePage: NextPage = () => {
     const ctx = canvasRef.current.getContext("2d");
     const image = new Image();
     image.crossOrigin = "Anonymous";
-    image.src = "./eng_bw.png";
+    image.src = "./text1.png";
     image.onload = async () => {
       ctx.drawImage(image, 0, 0);
       const {
         data: { text },
       } = await worker.recognize(image);
       console.log(`Text: ${text}`);
+      console.log(extract_clues(text))
     };
   }, []);
 
