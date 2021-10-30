00d88e4ed63b5042710699e4431688a879ac5e19
Xiaoning He <xhe123@hotmail.co.uk>
2021-10-30 16:01:16 +0100

    Import tesseract into React page

--- pages/text.tsx
+++ pages/text.tsx
@@ -2,34 +2,33 @@ import type { NextPage } from "next";
 import React, { useEffect, useRef } from "react";
 import Layout from "@/components/_Layout";
 import styles from "@/styles/Home.module.css";
-import Tesseract from "tesseract.js";
-import tess from "../services/tess"
+import { createWorker } from "tesseract.js";
 
 //@ts-ignore
 const ImagePage: NextPage = () => {
   const canvasRef = useRef(null);
 
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
+
   //@ts-ignore
   useEffect(async () => {
     console.log("loading...");
-    await tess.load();
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
     //@ts-ignore
     const ctx = canvasRef.current.getContext("2d");
     const image = new Image();
-    image.src = "https://tesseract.projectnaptha.com/img/eng_bw.png";
+    image.crossOrigin = "Anonymous";
+    image.src = "./eng_bw.png";
     image.onload = async () => {
       ctx.drawImage(image, 0, 0);
-      let imgData = ctx.getImageData(0, 0, 640, 425);
-      console.log(imgData);
-      console.log(
-        imgData.data.reduce((sum: number, val: number) => {
-          return sum + val;
-        }, 0)
-      );
-      // await tess.loadLanguage('eng');
-      // await tess.initialize('eng');
-      const text = tess.recognize_text(image.src);
-      console.log(text);
+      const {
+        data: { text },
+      } = await worker.recognize(image);
+      console.log(`Text: ${text}`);
     };
   }, []);
 
