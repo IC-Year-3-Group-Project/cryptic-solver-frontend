0ee24dda042f8a758e4f9f5154d8d06bc8f30e18
GuolinY <guolin.yang13@gmail.com>
2021-11-01 16:13:19 +0000

    Added cropping library and all components needed to crop an image. Cropping function implemented but not fully working

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,45 +4,96 @@ import { Grid } from "@mui/material";
 import { Button } from "@material-ui/core";
 import { Box } from "@mui/system";
 import { useState } from "react";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import Image from "next/image";
 
 export default function Upload() {
-  const [gridImg, setGridImg] = useState(null);
-  const [downImg, setDownImg] = useState(null);
-  const [acrossImg, setAcrossImg] = useState(null);
+  const [srcImg, setSrcImg] = useState(null);
+  const [image, setImage] = useState(null);
+  const [crop, setCrop] = useState({ aspect: 4 / 3 });
+  const [result, setResult] = useState(null);
+
+  const getCroppedImg = async () => {
+    try {
+      const canvas = document.createElement("canvas");
+      const scaleX = image.naturalWidth / image.width;
+      const scaleY = image.naturalHeight / image.height;
+      canvas.width = crop.width;
+      canvas.height = crop.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(
+        image,
+        crop.x * scaleX,
+        crop.y * scaleY,
+        crop.width * scaleX,
+        crop.height * scaleY,
+        0,
+        0,
+        crop.width,
+        crop.height
+      );
+
+      const base64Image = canvas.toDataURL("image/jpeg", 1);
+
+      setResult(base64Image);
+    } catch (e) {
+      console.log("crop the image");
+    }
+  };
 
   return (
     <Layout>
       <Grid
         container
-        direction="row"
+        direction="column"
         justifyContent="center"
         alignItems="center"
         spacing={2}
       >
         <Grid item>
-          <ImageUpload
-            title={"Upload image of the grid"}
-            img={gridImg}
-            setImg={setGridImg}
-          />
+          <Button variant="contained" component="label">
+            Upload image of a crossword
+            <input
+              type="file"
+              name="crosswordImage"
+              onChange={(e) => {
+                if (e.target.files && e.target.files[0]) {
+                  setSrcImg(e.target.files[0]);
+                } else {
+                  console.log("Could not upload image, please try again");
+                }
+              }}
+              hidden
+            />
+          </Button>
         </Grid>
-        <Grid item>
-          <ImageUpload
-            title={"Upload image of the across clues"}
-            img={downImg}
-            setImg={setDownImg}
-          />
-        </Grid>
-        <Grid item>
-          <ImageUpload
-            title={"Upload image of the down clues"}
-            img={acrossImg}
-            setImg={setAcrossImg}
-          />
-        </Grid>
+        {srcImg && (
+          <Grid item>
+            <ReactCrop
+              style={{ maxWidth: "30%" }}
+              src={URL.createObjectURL(srcImg)}
+              onImageLoaded={setImage}
+              crop={crop}
+              onChange={setCrop}
+            />
+          </Grid>
+        )}
+        {image && (
+          <Grid item>
+            <Button onClick={getCroppedImg}>crop</Button>
+          </Grid>
+        )}
+        {result && (
+          <Grid item>
+            <Image src={result} height={100} width={100} />
+          </Grid>
+        )}
       </Grid>
       <Box mt={5}>
-        {//TODO: make this button call image recognition function}
+        {
+          //TODO: make this button call image recognition function
+        }
         <Button variant="contained">Process Images</Button>
       </Box>
     </Layout>
