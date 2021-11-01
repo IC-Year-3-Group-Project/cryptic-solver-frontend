aad80661824e9dbfa4445d32f8be306df4cc537b
rodit <rodit9@gmail.com>
2021-11-01 14:08:13 +0000

    Add back to homepage button.

--- pages/crossword.tsx
+++ pages/crossword.tsx
@@ -9,6 +9,7 @@ import { Puzzle } from "@/components/Crossword/model/Puzzle";
 import { Box } from "@mui/system";
 import { convertEveryman, getCrossword } from "@/components/Crossword/utils";
 import NewCrossword from "@/components/Crossword/Crossword";
+import Button from "@material-ui/core/Button";
 
 const Crossword: NextPage = () => {
   const router = useRouter();
@@ -44,26 +45,29 @@ const Crossword: NextPage = () => {
   }, [router.query.url]);
 
   return (
-    <Layout>
-      {puzzle && (
-        <NewCrossword
-          puzzle={puzzle}
-          cellWidth={32}
-          cellHeight={32}
-        ></NewCrossword>
-      )}
-      {loadingCrossword && <CircularProgress />}
-      {fetchError && !loadingCrossword && (
-        <Box>
-          <Typography variant="h4" data-cy="sorry">
-            Sorry, your crossword could not be found!
-          </Typography>
-          <Link href="/">
-            <a data-cy="try-again">Try Again</a>
-          </Link>
-        </Box>
-      )}
-    </Layout>
+    <>
+      <Button style={{marginLeft: "2rem", marginTop: "2rem"}} onClick={() => router.push("/")}>🠐 Back</Button>
+      <Layout>
+        {puzzle && (
+          <NewCrossword
+            puzzle={puzzle}
+            cellWidth={32}
+            cellHeight={32}
+          ></NewCrossword>
+        )}
+        {loadingCrossword && <CircularProgress />}
+        {fetchError && !loadingCrossword && (
+          <Box>
+            <Typography variant="h4" data-cy="sorry">
+              Sorry, your crossword could not be found!
+            </Typography>
+            <Link href="/">
+              <a data-cy="try-again">Try Again</a>
+            </Link>
+          </Box>
+        )}
+      </Layout>
+    </>
   );
 };
 
