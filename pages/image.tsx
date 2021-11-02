753922c3a7ced17a8e80bb84a29de143c049b871
Zlatina Mileva <zlatina.mileva19@imperial.ac.uk>
2021-11-02 14:50:07 +0200

    Combine the 3 images and make sure filling clues is after loading all images.

--- pages/image.tsx
+++ pages/image.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Layout from "@/components/_Layout";
 import styles from "@/styles/Home.module.css";
 import cv from "../services/cv";
-import { createWorker } from "tesseract.js";
+import { createworker } from "tesseract.js";
 import { split } from "cypress/types/lodash";
 
 //@ts-ignore
@@ -17,8 +17,18 @@ const ImagePage: NextPage = () => {
   const [downHeight, setDownHeight] = useState(0)
   const [acrossWidth, setAcrossWidth] = useState(0)
   const [acrossHeight, setAcrossHeight] = useState(0)
+  const [acrossLoaded, setAcrossLoaded] = useState(false)
+  const [downLoaded, setDownLoaded] = useState(false)
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const [acrossClues, setAcrossClues] = useState([])
+  const [downClues, setDownClues] = useState([])
+  const [grid, setGrid] = useState({})
 
-  const worker = createWorker({
+  const workerAcross = createworker({
+    logger: (m: any) => console.log(m),
+  });
+
+  const workerDown = createworker({
     logger: (m: any) => console.log(m),
   });
 
@@ -97,11 +107,11 @@ const ImagePage: NextPage = () => {
     return clues;
   };
 
-  const fill_clues = (accross_clues: array, down_clues: array, grid: object) => {
+  const fill_clues = (across_clues: array, down_clues: array, grid: object) => {
     let grid_clues = grid.payload.clues
 
-    down_clues.forEach(clue => {
-      grid_clues.forEach(grid_clue => {
+    down_clues.forEach((clue: { clueNumber: string; clue: any; lengths: any[]; }) => {
+      grid_clues.forEach((grid_clue: { direction: number; number: number; text: any; lengths: any; }) => {
         if (grid_clue.direction == 1 && grid_clue.number == parseInt(clue.clueNumber)) {
           grid_clue.text = clue.clue
           grid_clue.lengths = clue.lengths.map(l => parseInt(l))
@@ -109,8 +119,8 @@ const ImagePage: NextPage = () => {
       });
     });
 
-    accross_clues.forEach(clue => {
-      grid_clues.forEach(grid_clue => {
+    across_clues.forEach((clue: { clueNumber: string; clue: any; lengths: any[]; }) => {
+      grid_clues.forEach((grid_clue: { direction: number; number: number; text: any; lengths: number[]; }) => {
         if (grid_clue.direction == 0 && grid_clue.number == parseInt(clue.clueNumber)) {
           grid_clue.text = clue.clue
           grid_clue.lengths = clue.lengths.map(l => parseInt(l))
@@ -125,43 +135,33 @@ const ImagePage: NextPage = () => {
   useEffect(async () => {
     console.log("loading...");
     await cv.load();
-    await worker.load();
-    await worker.loadLanguage("eng");
-    await worker.initialize("eng");
+    await workerAcross.load();
+    await workerAcross.loadLanguage("eng");
+    await workerAcross.initialize("eng");
+    await workerDown.load();
+    await workerDown.loadLanguage("eng");
+    await workerDown.initialize("eng");
+
     //@ts-ignore
-    const ctxGrid = canvasGridRef.current.getContext("2d");
-    const imageGrid = new Image();
-    imageGrid.src = "./grid_image.png";
+    const ctxAcrossClues = canvasAcrossCluesRef.current.getContext("2d");
+    const acrossClues = new Image();
+    acrossClues.src = "./across_clues.png";
+    acrossClues.onload = async () => {
+      ctxAcrossClues.width = acrossClues.width
+      ctxAcrossClues.height = acrossClues.height
+      setAcrossWidth(acrossClues.width)
+      setAcrossHeight(acrossClues.height)
+      ctxAcrossClues.drawImage(acrossClues, 0, 0);
+      const {
+        data: { text },
+      } = await workerDown.recognize(acrossClues);
+      console.log(`Text: ${text}`);
 
-    let result = {}
-
-    imageGrid.onload = async () => {
-      ctxGrid.width = imageGrid.width
-      ctxGrid.height = imageGrid.height
-      setGridWidth(imageGrid.width)
-      setGridHeight(imageGrid.height)
-      ctxGrid.drawImage(imageGrid, 0, 0);
-      let imgData = ctxGrid.getImageData(0, 0, imageGrid.width, imageGrid.height);
-      console.log(imgData);
-      console.log(
-        imgData.data.reduce((sum: number, val: number) => {
-          return sum + val;
-        }, 0)
-      );
-
-
-      await cv
-        .getGridFromImage(imgData)
-        .then((res) => {
-          console.log("Completed Promise");
-          console.log(res);
-          result = res
-        })
-        .catch((err) => {
-          console.log("Error occurred trying to process image");
-          console.log(err);
-        });
+      let clues = extract_clues(text)
+      setAcrossClues(clues)
+      setAcrossLoaded(true)
     };
+
 
     //@ts-ignore
     const ctxClues = canvasDownCluesRef.current.getContext("2d");
@@ -175,28 +175,56 @@ const ImagePage: NextPage = () => {
       ctxClues.drawImage(downClues, 0, 0);
       const {
         data: { text },
-      } = await worker.recognize(downClues);
+      } = await workerAcross.recognize(downClues);
       console.log(`Text: ${text}`);
 
       // DEBUG
-      let clues = extract_clues(text)
-      console.log("Down clues:");
-      console.log(clues)
+      let down_clues = extract_clues(text)
+      setDownClues(down_clues)
+      setDownLoaded(true)
+    };
 
-      console.log("Grid data:")
-      console.log(fill_clues([], clues, result.data));
-
-
+    //@ts-ignore
+    const ctxGrid = canvasGridRef.current.getContext("2d");
+    const imageGrid = new Image();
+    imageGrid.src = "./grid_image.png";
+    
+    imageGrid.onload = async () => {
+      ctxGrid.width = imageGrid.width
+      ctxGrid.height = imageGrid.height
+      setGridWidth(imageGrid.width)
+      setGridHeight(imageGrid.height)
+      ctxGrid.drawImage(imageGrid, 0, 0);
+      let imgData = ctxGrid.getImageData(0, 0, imageGrid.width, imageGrid.height);
+    
+      await cv
+        .getGridFromImage(imgData)
+        .then((res) => {
+          console.log("Completed Promise");
+          setGrid(res)
+          setImageLoaded(true)
+        })
+        .catch((err) => {
+          console.log("Error occurred trying to process image");
+          console.log(err);
+        });
     };
   }, []);
+
+  //@ts-ignore
+  useEffect(async () => {
+    if (acrossLoaded && downLoaded && imageLoaded){
+      console.log(fill_clues(acrossClues, downClues, grid.data))
+    }
+  }, [acrossLoaded, downLoaded, imageLoaded]);
 
   return (
     <Layout>
       <>
-        <div>
+      <div>
+          <canvas ref={canvasAcrossCluesRef} width={acrossWidth} height={acrossHeight} />
           <canvas ref={canvasGridRef} width={gridWidth} height={gridHeight} />
           <canvas ref={canvasDownCluesRef} width={downWidth} height={downHeight} />
-          <canvas ref={canvasAcrossCluesRef} width={acrossWidth} height={acrossHeight} />
         </div>
       </>
     </Layout>
