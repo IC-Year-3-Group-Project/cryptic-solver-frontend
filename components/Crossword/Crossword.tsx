04a78543683d68df8004240d9d2bced70f4945b1
Xiaoning He <xh1019@ic.ac.uk>
2022-01-18 16:53:43 +0000

    Update regex matching for slash

--- components/Crossword/Crossword.tsx
+++ components/Crossword/Crossword.tsx
@@ -443,7 +443,7 @@ export default function Crossword(props: CrosswordProps) {
         async function tryLoadMorseExplanation() {
           try {
             const explanation = await getExplanation(
-              clue.getClueText().replace(/\\\//g, ""),
+              clue.getClueText().replace(/[\\/]/g, ""),
               text.toUpperCase()
             );
             if (explanation.trim().length > 0) {
@@ -528,7 +528,7 @@ export default function Crossword(props: CrosswordProps) {
     try {
       const filledInAnswer = getClueText(clue).replace(/_/g, "?");
       // Strip html tags and word length brackets from clue.
-      const strippedClue = clue.getClueText().replace(/\\\//g, "");
+      const strippedClue = clue.getClueText().replace(/[\\/]/g, "");
       const pattern = clue.getSolutionPattern();
       let solutions = solutionCache[clue.getTitle()];
       if (!solutions) {
@@ -642,7 +642,7 @@ export default function Crossword(props: CrosswordProps) {
       let hints: string[] = [];
       if (!clue.explanation) {
         clue.explanation = await getExplanation(
-          clue.getClueText().replace(/\\\//g, ""),
+          clue.getClueText().replace(/[\\/]/g, ""),
           clue.solution
         );
         if (!clue.explanation) {
@@ -760,7 +760,7 @@ export default function Crossword(props: CrosswordProps) {
     setLoadingSolution(true);
 
     try {
-      const strippedClue = clue.getClueText().replace(/\\\//g, "");
+      const strippedClue = clue.getClueText().replace(/[\\/]/g, "");
       const explanation = await getExplanation(
         strippedClue,
         answer,
@@ -1483,7 +1483,7 @@ export default function Crossword(props: CrosswordProps) {
         solutions={solutions}
         currentText={getClueText()}
         anchor={solveOverlayTarget?.current ?? solutionMenuTarget?.current}
-        onSolutionSelected={onSolutionSelected}
+        onSolutionSelected={onSolutionSelected}.
       />
       <Dialog open={explanation != undefined}>
         <DialogTitle>Explanation for {selectedClue?.getTitle()}</DialogTitle>
